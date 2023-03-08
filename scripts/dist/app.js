cde14bde33219dcfd5e41b0703f5feb6a5300cc4
anmedd-osama <ahmed.ossama10@gmail.com>
2023-03-08 18:05:55 +0200

    commiting changes of the landing page image swiper

--- scripts/dist/app.js
+++ scripts/dist/app.js
@@ -2,3 +2,14 @@
 // navigation bar
 let navItems = document.querySelectorAll("nav .bar ul li.nav-item a");
 navItems.forEach(item => item.addEventListener('click', (e) => { navItems.forEach(item => item.classList.remove('active')); e.target.classList.add('active'); }));
+// landing
+const swiper = new Swiper('.sneaker-swiper .swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    effect: "flip",
+});
--- scripts/src/app.ts
+++ scripts/src/app.ts
@@ -1,3 +1,14 @@
 // navigation bar
 let navItems = document.querySelectorAll("nav .bar ul li.nav-item a")
 navItems.forEach(item=> item.addEventListener('click',(e:any)=>{navItems.forEach(item=>item.classList.remove('active')); e.target.classList.add('active'); }));
+// landing
+const swiper = new Swiper('.sneaker-swiper .swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  effect: "flip",
+});
